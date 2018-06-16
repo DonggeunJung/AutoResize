01e3688d8d49698ce35a76c86c3b23b6a252853e
DonggeunJung <topsan72@gmail.com>
2018-06-16 12:06:34 -0400

    Initial commit

---
+++ AutoResizeEx/BaseViewController.swift
@@ -0,0 +1,45 @@
+//
+//  BaseViewController.swift
+//  AutoResizeEx
+//
+//  Created by 정동근 on 2018. 6. 16..
+//  Copyright © 2018년 Dennis Jung. All rights reserved.
+//
+
+import UIKit
+
+class BaseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        resizeAllViews_iP8()
+    }
+    
+    // Resize All Views Automately (Default Screen size is iPhone-8)
+    func resizeAllViews_iP8() {
+        for subView in view.subviews {
+            resizeView_iP8( subView )
+        }
+    }
+    
+    // Resize One View Automately (Default Screen size is iPhone-8)
+    func resizeView_iP8(_ ctrl: UIView) {
+        let iP8Width: CGFloat = 375.0
+        let iP8Height: CGFloat = 667.0
+        let thisWidth: CGFloat = view.frame.size.width
+        let thisHeight: CGFloat = view.frame.size.height
+        let rateW = thisWidth / iP8Width
+        let rateH = thisHeight / iP8Height
+        
+        let ctrlX = ctrl.frame.origin.x * rateW
+        let ctrlY = ctrl.frame.origin.y * rateH
+        let ctrlWidth = ctrl.frame.size.width * rateW
+        let ctrlHeight = ctrl.frame.size.height * rateH
+        
+        ctrl.frame.origin.x = ctrlX
+        ctrl.frame.origin.y = ctrlY
+        ctrl.frame.size.width = ctrlWidth
+        ctrl.frame.size.height = ctrlHeight
+    }
+}
